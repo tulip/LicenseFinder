45135640197657a80ea04f1938e6cdf03c509cce
Ryan Collins <rcollins@pivotal.io>
2018-02-06 16:17:51 -0500

    Remove ssh handling from CI pushscript
    Signed-off-by: Daniil Kouznetsov <dkouznetsov@pivotal.io>

--- ci/scripts/pushscript.sh
+++ ci/scripts/pushscript.sh
@@ -10,13 +10,6 @@ built_gem="pkg/license_finder-$build_version.gem"
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_USERNAME
 
-mkdir ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-eval "$(ssh-agent -s)"
-echo "$GIT_PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-ssh-add -k ~/.ssh/id_rsa
-
 if [ -z "$(gem fetch license_finder -v $build_version 2>&1 | grep ERROR)" ]; then
   echo "LicenseFinder-$build_version already exists on Rubygems"
 else
@@ -25,6 +18,4 @@ else
   rake release:rubygem_push
 fi
 
-export EXIT_STATUS=$?
-kill $(ps aux | grep ssh-agent | head -n 1 | awk '{print $2}')
-exit $EXIT_STATUS
+exit $?
